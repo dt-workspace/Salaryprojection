26eae81583edcfbbd2096aa4380687d24d302c6a
amitsoni <114071792+amitsonit@users.noreply.github.com>
2023-07-17 23:43:46 +0530

    Case-InFlow and Case out flow

--- App.tsx
+++ App.tsx
@@ -8,12 +8,13 @@ import {
   useColorScheme,
   View,
   Dimensions,
-  FlatList,
+  Switch,
 } from "react-native";
 
 import { formatSalary } from "./src/utility/salaryFormater";
 import SimpleInput from "./src/components/FormInput/SimpleInput";
 import LineText from "./src/components/LineText";
+import { FlashList } from "@shopify/flash-list";
 
 const { width, height } = Dimensions.get("window");
 
@@ -23,6 +24,14 @@ type themeProps = {
   backgroundColor: string;
   onBackgroundColor: string;
   textColor: string;
+};
+
+type caseFlow = {
+  salary: number;
+  isTarget: boolean;
+  isOnTime: boolean;
+  year: number;
+  yearly: number;
 };
 
 function App(): JSX.Element {
@@ -43,19 +52,23 @@ function App(): JSX.Element {
 
   const [salary, setSalary] = useState<number>(22000);
   const [hike, setHike] = useState<number>(36);
-  const [targetYear, setTargetYear] = useState<number>(40);
+  const [targetYear, setTargetYear] = useState<number>(17);
   const currentDate = new Date();
+  const [expense, setExpense] = useState<number>(2000);
+  const [inflation, setInflation] = useState<number>(6);
+  const [OutflowEnabled, setOutflowEnabled] = useState<boolean>(false);
 
   let Salary = useCallback(() => {
-    let list = [];
+    let list = [] as caseFlow[];
     let a = (hike + 100) / 100;
     for (let i = 1; i <= targetYear; i++) {
       let newSalary = (salary * Math.pow(a, i)).toFixed();
       list.push({
-        salary: newSalary,
+        salary: parseInt(newSalary),
         isTarget: false,
         isOnTime: false,
         year: currentDate.getFullYear() + i,
+        yearly: parseInt(newSalary) * 12,
       });
     }
 
@@ -65,9 +78,7 @@ function App(): JSX.Element {
     let nextThresholdYears = [2040, 2060, 2080, 2100, 2120];
 
     for (const [index, threshold] of nextThresholds.entries()) {
-      let nextThreshold = list.findIndex(
-        (item) => parseInt(item.salary) > threshold
-      );
+      let nextThreshold = list.findIndex((item) => item.salary > threshold);
       if (nextThreshold != -1) {
         list[nextThreshold] = {
           ...list[nextThreshold],
@@ -77,28 +88,88 @@ function App(): JSX.Element {
       }
     }
 
+    let outflow = list.map((out, iout) => {
+      let yearly =
+        out.yearly -
+        (expense * 12 + ((expense * 12 * inflation) / 100) * (iout + 1));
+      let monthly =
+        out.salary - (expense + ((expense * inflation) / 100) * (iout + 1));
+      
+      return {
+        ...out,
+        salary: monthly,
+        yearly: yearly,
+      };
+    });
+
     return {
-      salaries: list,
+      salaries: OutflowEnabled ? outflow : list,
     };
-  }, [hike, salary, targetYear]);
+  }, [hike, salary, targetYear, expense, inflation, OutflowEnabled]);
 
   let { salaries } = Salary();
 
   const FooterComponent = useCallback(() => {
     return (
-      <View style={{ height: 100, justifyContent: "center" }}>
-        <Text style={{ textAlign: "center", color: style.textColor }}>
-          You reached end of the List
-        </Text>
+      <View>
+        <LineText
+          text={"Cash Out-Flow"}
+          textStyle={style.centerText}
+          lineStyle={style.centerBorder}
+        />
+        <View style={{ alignItems: "center",marginBottom:10 }}>
+          <Switch
+            trackColor={{ false: 'rgba(243,243,243,.2)', true: "#f4f3f4"}}
+            thumbColor={OutflowEnabled ? 'green' : "#f4f3f4"}
+            onValueChange={() => {
+              setOutflowEnabled(!OutflowEnabled);
+            }}
+            value={OutflowEnabled}
+            
+          />
+        </View>
+
+        <View style={[style.salaryInputContainer,{elevation:4}]}>
+          <View style={{ alignItems: "center" }}>
+            <Text style={style.inputLabel}>Expense</Text>
+            <SimpleInput
+              defaultValue={expense.toString()}
+              customStyle={style.inputBoxLeft}
+              keyboardType="number-pad"
+              placeholder="2000"
+              maxLength={10}
+              placeholderTextColor={"gray"}
+              onChangeText={(text) =>
+                setExpense(text.length > 0 ? parseInt(text) : 0)
+              }
+            />
+          </View>
+          <View style={{ alignItems: "center" }}>
+            <Text style={style.inputLabel}>Inflation</Text>
+
+            <SimpleInput
+              defaultValue={inflation.toString()}
+              customStyle={[style.inputBoxCenter]}
+              keyboardType="number-pad"
+              placeholder="7"
+              placeholderTextColor={"gray"}
+              maxLength={3}
+              onChangeText={(text) =>
+                setInflation(text.length > 0 ? parseInt(text) : 0)
+              }
+            />
+          </View>
+        </View>
+        <View style={{ height: 50 }}></View>
       </View>
     );
-  }, []);
+  }, [OutflowEnabled]);
 
   const HeaderComponent = useCallback(() => {
     return (
       <View>
         <LineText
-          text={"Salary"}
+          text={"Cash In-flow"}
           textStyle={style.centerText}
           lineStyle={style.centerBorder}
         />
@@ -149,16 +220,12 @@ function App(): JSX.Element {
             />
           </View>
         </View>
-        <LineText
-          text={"Targets"}
-          textStyle={style.centerText}
-          lineStyle={style.centerBorder}
-        />
+        <View style={{height:30}}></View>
       </View>
     );
-  }, []);
+  }, [style]);
 
-  const SalaryItemComponent = useCallback(({ item, index }) => {
+  const SalaryItemComponent = useCallback(({ item, index }:{item:caseFlow,index:number}) => {
     return (
       <Pressable
         key={index}
@@ -184,7 +251,7 @@ function App(): JSX.Element {
               color: item.isTarget ? "white" : style.textColor,
             }}
           >
-            ₹ {formatSalary(parseInt(item.salary))}
+            ₹ {formatSalary(item.salary)}
           </Text>
         </View>
         <View style={{ flexDirection: "row", alignItems: "center" }}>
@@ -200,18 +267,19 @@ function App(): JSX.Element {
               marginLeft: 10,
             }}
           >
-            ₹ {formatSalary(parseInt(item.salary) * 12)}
+            ₹ {formatSalary(item.yearly)}
           </Text>
         </View>
       </Pressable>
     );
-  }, []);
+  }, [style]);
 
   return (
     <View style={style.root}>
       <StatusBar hidden={true} />
-      <FlatList
-        style={style.salaryResultContainer}
+      <FlashList
+        estimatedItemSize={17}
+        contentContainerStyle={style.salaryResultContainer}
         showsVerticalScrollIndicator={false}
         data={salaries}
         keyExtractor={(item, index) => index.toString()}
@@ -230,10 +298,10 @@ const createStyle = (T: themeProps) =>
       backgroundColor: T.backgroundColor,
     },
     centerBorder: {
-      borderWidth: 1,
-      borderColor: "rgba(51,51,51,.5)",
-      marginRight: 5,
-      width: "45%",
+      // borderWidth: 1,
+      // borderColor: "rgba(51,51,51,.1)",
+      // marginRight: 5,
+      // width: "45%",
     },
     centerText: {
       textAlign: "center",
@@ -281,7 +349,7 @@ const createStyle = (T: themeProps) =>
       borderBottomColor: T.onPrimaryColor,
       borderBottomLeftRadius: 20,
     },
-    inputBoxCenter:{
+    inputBoxCenter: {
       textAlign: "center",
       marginTop: 10,
       color: T.onPrimaryColor,
